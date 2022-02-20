1581f2b83d818dddbd1fb7bc49203dca41228ce3
Holger Oehm <holger.oehm@holger-oehm.de>
2022-02-20 10:21:23 +0100

    Print info about important hosts

---
+++ serverinfo.js
@@ -0,0 +1,18 @@
+var known;
+var path;
+var hackingLevel;
+var bestVictim;
+var hackScript = "hack-server.script";
+var hackSelf = true;
+
+/** @param {NS} ns **/
+export async function main(ns) {
+	ns.tprintf("%30s %10s %10s %10s", "Server", "Nuked", "Backdoor", "Hacklevel");
+	for (var server of [ "CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "The-Cave", "w0r1d_d43m0n" ]) {
+		var info = ns.getServer(server);
+		ns.tprintf("%30s %10s %10s %10d", server,
+			info.hasAdminRights ? "X" : "",
+			info.backdoorInstalled ? "X" : "",
+			info.requiredHackingSkill);
+	}
+}
