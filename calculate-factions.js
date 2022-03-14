0e7f433146c4f395c1d0a8a143684b78fd86913a
Holger Oehm <holger.oehm@holger-oehm.de>
2022-03-14 12:55:10 +0100

    More dynamic goals

--- calculate-factions.js
+++ calculate-factions.js
@@ -1,75 +1,77 @@
 import * as c from "constants.js";
 
-const STORY_LINE = [
-	c.CYBERSEC,
-	c.NETBURNERS,
-	c.SECTOR12,
-	c.NITESEC,
-	c.BLACK_HAND,
-	c.CHONGQING,
-	c.TIAN_DI_HUI,
-	c.BITRUNNERS,
-	c.DAEDALUS].reverse();
-// const CITIES = [c.SECTOR12, c.AEVUM, c.ISHIMA, c.CHONGQING, c.NEW_TOKYO, c.VOLHAVEN];
 
-const AUGS_BEFORE_INSTALL = 7;
-const AUGS_PER_FACTION = 2;
+// Faction type:
+// { name, backdoor, work, location }
+
+const STORY_LINE = [
+	{ name: c.CYBERSEC, backdoor: "CSEC", work: c.HACKING, location: "" },
+	{ name: c.NETBURNERS, backdoor: "", work: c.HACKING, location: "" },
+	{ name: c.SECTOR12, backdoor: "", work: c.HACKING, location: c.SECTOR12 },
+	{ name: c.NITESEC, backdoor: "avmnite-02h", work:c.HACKING, location:"" },
+	{ name: c.BLACK_HAND, backdoor: "I.I.I.I", work:c.HACKING, location:"" },
+	{ name: c.CHONGQING, backdoor: "", work:c.HACKING, location: c.CHONGQING },
+	{ name: c.TIAN_DI_HUI, backdoor: "", work:c.HACKING, location: c.CHONGQING },
+	{ name: c.BITRUNNERS, backdoor: "run4theh111z", work: c.HACKING, location: "" },
+	{ name: c.DAEDALUS, backdoor: "", work: c.HACKING, location: "" }
+	].reverse();
+
+const AUGS_BEFORE_INSTALL = 8;
+const AUGS_PER_FACTION = 3;
 
 /** @param {NS} ns **/
 export async function main(ns) {
-	var factionsToJoin = [];
-	var augmentations = new Set();
-	for (var augmentation of ns.getOwnedAugmentations(true)) {
-		augmentations.add(augmentation);
-	}
-	for (var faction of STORY_LINE) {
-		if (hasNewAugmentation(ns, faction, augmentations)) {
-			factionsToJoin.push(faction);
-		}
-	}
-	factionsToJoin.reverse();
-	ns.tprintf("Factions to join: %s", JSON.stringify(factionsToJoin));
+	var faction_augmentations = [];
 	var ownedAugmentations = ns.getOwnedAugmentations(true);
+	buildDatabase(ns, faction_augmentations, STORY_LINE, ownedAugmentations.slice(0));
+
+	ns.tprintf("Database of factions and augmentations: %s", JSON.stringify(faction_augmentations));
+
+	var factionsToJoin = [];
+	for (var faction of faction_augmentations) {
+		factionsToJoin.push(faction.name);
+	}
+	ns.tprintf("Factions to join: %s", JSON.stringify(factionsToJoin));
+
+	var faction_goals = [];
 	var newAugs = 0;
-	var factionReps = [];
-	for (var faction of factionsToJoin) {
-		var augmentations = ns.getAugmentationsFromFaction(faction).filter(
-			(a) => !ownedAugmentations.includes(a));
-		ns.tprintf("Augmentations owned: %s", JSON.stringify(ownedAugmentations));
-		ns.tprintf("Factions from %s: %s", faction, JSON.stringify(augmentations));
-		augmentations.sort((a, b) => ns.getAugmentationRepReq(a) - ns.getAugmentationRepReq(b));
-		var repToReach = augmentations.length > AUGS_PER_FACTION ?
-			ns.getAugmentationRepReq(augmentations[AUGS_PER_FACTION]) :
-			ns.getAugmentationRepReq(augmentations.pop());
-		for (var augmentation of augmentations) {
-			if (ns.getAugmentationRepReq(augmentation) <= repToReach) {
+	for (var faction of faction_augmentations) {
+		var augsToAdd = Math.min(AUGS_PER_FACTION, AUGS_BEFORE_INSTALL - newAugs);
+		var repToReach = faction.augmentations.length >= augsToAdd ?
+			faction.augmentations[augsToAdd - 1].reputation :
+			faction.augmentations[faction.augmentations.length - 1].reputation;
+		for (var augmentation of faction.augmentations) {
+			if (augmentation.reputation <= repToReach) {
 				newAugs++;
-				ownedAugmentations.push(augmentation);
-			}
-			if (newAugs >= AUGS_BEFORE_INSTALL) {
-				repToReach = ns.getAugmentationRepReq(augmentation);
-				break;
 			}
 		}
-		if (ns.getFactionRep(faction) < repToReach) {
-			factionReps.push({ name: faction, reputation: repToReach });
+		if (ns.getFactionRep(faction.name) < repToReach) {
+			faction_goals.push({ name: faction.name, properties:faction.properties, reputation: repToReach });
 		}
 		if (newAugs >= AUGS_BEFORE_INSTALL) {
 			break;
 		}
 	}
-	ns.tprintf("Faction goals: %s", JSON.stringify(factionReps));
-	await ns.write("nodestart.txt", JSON.stringify({ toJoin: factionsToJoin, factionGoals: factionReps }), "w");
+	ns.tprintf("Faction goals: %s", JSON.stringify(faction_goals));
+	await ns.write("nodestart.txt", JSON.stringify({ toJoin: factionsToJoin, factionGoals: faction_goals }), "w");
 }
 
 /** @param {NS} ns **/
-function hasNewAugmentation(ns, faction, augmentations) {
-	var hasNew = false;
-	for (var augmentation of ns.getAugmentationsFromFaction(faction)) {
-		if (!augmentations.has(augmentation)) {
-			hasNew = true;
-			augmentations.add(augmentation);
+function buildDatabase(ns, faction_augmentations, factions, ignore) {
+	for (var faction of factions) {
+		var augmentations = ns.getAugmentationsFromFaction(faction.name).filter(a => !ignore.includes(a));
+		if (augmentations.length > 0) {
+			var augmentations_with_rep = [];
+			for (var augmentation of augmentations) {
+				augmentations_with_rep.push({
+					augmentation: augmentation,
+					reputation: ns.getAugmentationRepReq(augmentation)
+				});
+			}
+			augmentations_with_rep.sort((a, b) => a.reputation - b.reputation);
+			faction_augmentations.push({ name: faction.name, properties:faction, augmentations: augmentations_with_rep });
+			ignore = ignore.concat(augmentations);
 		}
 	}
-	return hasNew;
+	faction_augmentations.reverse();
 }
--- nodestart.js
+++ nodestart.js
@@ -7,112 +7,40 @@ export async function main(ns) {
 	const config = JSON.parse(ns.read("nodestart.txt"));
 	ns.tprintf("Config: %s", JSON.stringify(config));
 
-	var bootcount = await getCounter(ns);
-	ns.tprintf("Started %d. run", +bootcount + 1);
-	await firstActions(ns, bootcount);
+	if (ns.getPlayer().hacking < 100) {
+		await firstActions(ns);
+	}
 
 	for (var goal of config.factionGoals) {
-		switch (goal.name) {
-			case c.NITESEC:
-				await runAndWait(ns, "commit-crimes.js", getHacklevel(ns, "avmnite-02h"));
-				await runAndWait(ns, "rscan.js", "hack");
-				await runAndWait(ns, "rscan.js", "back");
-				break;
-			case c.BLACK_HAND:
-				await runAndWait(ns, "writeprogram.js", 2);
+		ns.tprintf("Next goal: %s", JSON.stringify(goal));
+		if (goal.properties.backdoor) {
+			var backdoor = goal.properties.backdoor;
+			if (!ns.getServer(backdoor).backdoorInstalled) {
+				var hackLevel_needed = ns.getServerRequiredHackingLevel(backdoor);
+				await runAndWait(ns, "commit-crimes.js", hackLevel_needed);
+				var ports_needed = ns.getServerNumPortsRequired(backdoor);
+				if (ports_needed > 0) {
+					await runAndWait(ns, "writeprogram.js", ports_needed - 1);
+				}
 				await startHacking(ns);
-				await runAndWait(ns, "commit-crimes.js", getHacklevel(ns, "I.I.I.I")); // Black Hand
-				await runAndWait(ns, "rscan.js", "hack");
-				await runAndWait(ns, "rscan.js", "back");
-				break;
+			}
 		}
-		await workForFactionUntil(ns, config.toJoin, goal.name, c.HACKING, goal.reputation);
+		if (goal.properties.location) {
+			ns.travelToCity(goal.properties.location);
+		}
+		await workForFactionUntil(ns, config.toJoin, goal.name, goal.properties.work, goal.reputation);
 		await runAndWait(ns, "solve_contract.js", "auto");
 	}
 	ns.spawn("plan-augmentations.js");
-
-	return;
-
-	await runAndWait(ns, "start-hacknet.js", bootcount);
-
-	if (bootcount < 8) {
-		await runAndWait(ns, "commit-crimes.js", getHacklevel(ns, "I.I.I.I")); // Black Hand
-		await runAndWait(ns, "rscan.js", "hack");
-		await runAndWait(ns, "rscan.js", "back");
-	}
-	await runAndWait(ns, "writeprogram.js", 3);
-	await startHacking(ns);
-	if (bootcount < 9) {
-		await runAndWait(ns, "commit-crimes.js", getHacklevel(ns, "run4theh111z")); // BitRunners
-		await runAndWait(ns, "rscan.js", "hack");
-		await runAndWait(ns, "rscan.js", "back");
-	}
-	await runAndWait(ns, "start-servers.js", 512);
-
-	switch (bootcount) {
-		case 6:
-			await workForFactionUntil(ns, wantedFactions, c.BLACK_HAND, c.HACKING, 50000);
-			await workForFactionUntil(ns, wantedFactions, c.BITRUNNERS, c.HACKING, 100000);
-			await runAndWait(ns, "solve_contract.js", "auto");
-			ns.spawn("plan-augmentations.js");
-			break;
-		case 7:
-			await workForFactionUntil(ns, wantedFactions, c.BLACK_HAND, c.HACKING, 100000);
-			await workForFactionUntil(ns, wantedFactions, c.BITRUNNERS, c.HACKING, 200000);
-			await runAndWait(ns, "solve_contract.js", "auto");
-			ns.spawn("plan-augmentations.js");
-			break;
-		case 8:
-			await workForFactionUntil(ns, [], c.BITRUNNERS, c.HACKING, 1000000);
-			await runAndWait(ns, "solve_contract.js", "auto");
-			ns.spawn("plan-augmentations.js");
-			break
-	}
-	await runAndWait(ns, "writeprogram.js", 4);
-	await startHacking(ns);
-	await runAndWait(ns, "solve_contract.js", "auto");
-
-	switch (bootcount) {
-		case 9:
-			await workForFactionUntil(ns, [], c.DAEDALUS, c.HACKING, 1000000);
-			await runAndWait(ns, "solve_contract.js", "auto");
-			ns.spawn("plan-augmentations.js");
-			break
-	}
 }
 
 /** @param {NS} ns **/
-async function getCounter(ns) {
-	var player = ns.getPlayer();
-	if (player.playtimeSinceLastAug == player.playtimeSinceLastBitnode) {
-		ns.rm("count.txt");
-	}
-	var bootcount = ns.read("count.txt");
-	if (bootcount == "") {
-		bootcount = 0;
-	}
-	await ns.write("count.txt", bootcount, "w");
-
-	return +bootcount;
-}
-
-/** @param {NS} ns **/
-async function writeCounter(ns, bootcount) {
-	await ns.write("count.txt", bootcount, "w");
-}
-
-/** @param {NS} ns **/
-async function firstActions(ns, bootcount) {
+async function firstActions(ns) {
 	if (ns.getServer("home").maxRam > 32) {
 		if (!ns.scriptRunning("instrument.script", "home")) {
 			ns.run("instrument.script", 1, "foodnstuff");
 		}
 	}
-	if (bootcount < 6) {
-		await runAndWait(ns, "start-hacknet.js", bootcount);
-	}
-	await runAndWait(ns, "solve_contract.js", "auto");
-	if (ns.getPlayer().hacking > 100) return;
 
 	// nuke and hack unprotected hosts
 	await startHacking(ns);
@@ -122,23 +50,6 @@ async function firstActions(ns, bootcount) {
 	await runAndWait(ns, "writeprogram.js", 0);
 	// now use it
 	await startHacking(ns);
-	while (!ns.getServer("CSEC").backdoorInstalled) {
-		await runAndWait(ns, "commit-crimes.js", getHacklevel(ns, "CSEC"));
-		await runAndWait(ns, "rscan.js", "back");
-		await ns.sleep(5000);
-	}
-	// if we are going to join CyberSec later on anyway, do it right now
-	if (bootcount < 3) {
-		await runAndWait(ns, "workforfaction.js", 1, c.CYBERSEC, c.HACKING, JSON.stringify([c.CYBERSEC, c.NETBURNERS]));
-	} else {
-		// don't join unecessarily a faction
-		await runAndWait(ns, "commit-crimes.js", 100);
-	}
-
-	// do the second program, too
-	await runAndWait(ns, "writeprogram.js", 1);
-	await startHacking(ns);
-	await runAndWait(ns, "solve_contract.js", "auto");
 }
 
 /** @param {NS} ns **/
