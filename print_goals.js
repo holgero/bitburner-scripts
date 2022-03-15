20e4e64de4e2ba27cd1bde1cda65897a92d703a2
Holger Oehm <holger.oehm@holger-oehm.de>
2022-03-15 22:53:37 +0100

    Print current goals human readable

---
+++ print_goals.js
@@ -0,0 +1,8 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+	const config = JSON.parse(ns.read("nodestart.txt"));
+	ns.tprintf("%20s %20s %20s", "Faction", "Reputation Goal", "Current Reputation");
+	for (var goal of config.factionGoals) {
+		ns.tprintf("%20s %20d %20d", goal.name, goal.reputation, ns.getFactionRep(goal.name));
+	}
+}
