957c1bb04600fa466046d8d86baef7c778d8cab2
Holger Oehm <holger.oehm@holger-oehm.de>
2022-03-22 21:28:34 +0100

    More space for long faction names

--- print_goals.js
+++ print_goals.js
@@ -1,15 +1,15 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	const config = JSON.parse(ns.read("nodestart.txt"));
-	ns.tprintf("%20s %20s %20s", "Faction", "Reputation Goal", "Current Reputation");
+	ns.tprintf("%22s %20s %20s", "Faction", "Reputation Goal", "Current Reputation");
 	for (var goal of config.factionGoals) {
-		ns.tprintf("%20s %20d %20d", goal.name, goal.reputation, ns.getFactionRep(goal.name));
+		ns.tprintf("%22s %20d %20d", goal.name, goal.reputation, ns.getFactionRep(goal.name));
 		var idx = config.toJoin.indexOf(goal.name);
 		if (idx >= 0) {
 			config.toJoin.splice(idx, 1);
 		}
 	}
 	for (var faction of config.toJoin) {
-		ns.tprintf("%20s %20s %20d", faction, "", ns.getFactionRep(faction));
+		ns.tprintf("%22s %20s %20d", faction, "", ns.getFactionRep(faction));
 	}
 }
