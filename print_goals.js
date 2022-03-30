94da83677b11ea43b7f700a32f67a99f17a25285
Holger Oehm <holger.oehm@holger-oehm.de>
2022-03-30 17:48:56 +0200

    Print goal completion percentage

--- print_goals.js
+++ print_goals.js
@@ -1,8 +1,11 @@
 /** @param {NS} ns **/
 export async function main(ns) {
 	const config = JSON.parse(ns.read("nodestart.txt"));
-	ns.tprintf("%22s %20s %20s", "Faction", "Reputation Goal", "Current Reputation");
+	ns.tprintf("%22s %20s %20s %10s", "Faction", "Reputation Goal",
+		"Current Reputation", "Completion");
 	for (var goal of config.factionGoals) {
-		ns.tprintf("%22s %20d %20d", goal.name, goal.reputation, ns.getFactionRep(goal.name));
+		ns.tprintf("%22s %20d %20d %10s", goal.name, goal.reputation,
+		ns.getFactionRep(goal.name),
+		goal.reputation ? (100*ns.getFactionRep(goal.name)/goal.reputation).toFixed(1) + " %":"");
 	}
 }
