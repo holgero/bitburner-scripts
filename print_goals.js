243ee1ffc9ad48cf2a18d67a6bb6f7a7f3eed2fe
Holger Oehm <holger.oehm@holger-oehm.de>
2022-04-04 17:46:07 +0200

    Print augmentations for the factions

--- print_goals.js
+++ print_goals.js
@@ -3,13 +3,30 @@ import { formatMoney } from "helpers.js";
 /** @param {NS} ns **/
 export async function main(ns) {
 	const config = JSON.parse(ns.read("nodestart.txt"));
-	ns.tprintf("%22s %20s %20s %10s", "Faction", "Reputation Goal",
-		"Current Reputation", "Completion");
+	ns.tprintf("%22s %20s %20s %10s  %s", "Faction", "Reputation Goal",
+		"Current Reputation", "Completion", "Augmentations");
 	for (var goal of config.factionGoals) {
-		ns.tprintf("%22s %20d %20d %10s", goal.name, goal.reputation,
-		ns.getFactionRep(goal.name),
-		goal.reputation ? (100*ns.getFactionRep(goal.name)/goal.reputation).toFixed(1) + " %":"");
+		if (goal.reputation) {
+			ns.tprintf("%22s %20d %20d %10s  %s",
+				goal.name,
+				goal.reputation,
+				ns.getFactionRep(goal.name),
+				(100 * ns.getFactionRep(goal.name) / goal.reputation).toFixed(1) + " %",
+				augmentationsFrom(goal));
+		} else {
+			ns.tprintf("%22s %20s %20d", goal.name, "", ns.getFactionRep(goal.name));
+		}
 	}
 	ns.tprintf("Estimated costs: augmentations: %s, donations: %s",
 		formatMoney(config.estimatedPrice), formatMoney(config.estimatedDonations));
+}
+
+function augmentationsFrom(goal) {
+	var result = "";
+	for (var augmentation of goal.augmentations) {
+		if (augmentation.reputation <= goal.reputation) {
+			result = result + augmentation.augmentation.substring(0, 12) + ", ";
+		}
+	}
+	return result.substring(0, result.length - 2);
 }
