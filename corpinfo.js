b857e697e56ffa7726ca10091a6cbecd55ef8d43
Holger Oehm <holger.oehm@holger-oehm.de>
2022-04-10 16:21:08 +0200

    Add utility to print current corporation info (incl. cooldown)

---
+++ corpinfo.js
@@ -0,0 +1,17 @@
+import { formatMoney } from "./helpers.js";
+
+/** @param {NS} ns */
+export async function main(ns) {
+	var corporationInfo = JSON.parse(ns.read("corporation.txt"));
+	var low = corporationInfo.valuation / (2 * corporationInfo.totalShares);
+	var high = corporationInfo.valuation / (2 * (corporationInfo.totalShares -
+		corporationInfo.issuedShares - corporationInfo.numShares));
+	var profit = corporationInfo.revenue - corporationInfo.expenses;
+
+	ns.tprintf("Corporation: share=%s (%s-%s), funds=%s, profit=%s, cool=%d s, owned=%s",
+		formatMoney(corporationInfo.sharePrice), formatMoney(low), formatMoney(high),
+		formatMoney(corporationInfo.funds),
+		formatMoney(profit),
+		Math.ceil(corporationInfo.shareSaleCooldown / 5),
+		corporationInfo.issuedShares == 0 ? "*" : "-");
+}
