1f8f6dfdbbae15de79c47088dedb8efc670d9a54
Holger Oehm <holger.oehm@holger-oehm.de>
2022-05-07 10:00:21 +0200

    Add check-git-pull.sh as a pre-commit hook

--- check-git-pull.sh
+++ check-git-pull.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-(
+MISSING=$( (
 	sed -n '2,/CONTRACTSOLVER/s/^[ \t]*"\(\S*\)"[],;]*/\1/p' git-pull.js
 	ls *.js
-) | sort | uniq -c | grep -v ' 2 '
+) | sort | uniq -c | grep -v ' 2 ')
+
+if [ -n "$MISSING" ] ; then
+  echo "Files are missing from git-pull.js:" $MISSING
+  exit 1
+fi
