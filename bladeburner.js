22a703d84a89fe9ec26b84ef8c09787021cac4a6
Holger Oehm <holger.oehm@holger-oehm.de>
2022-05-27 08:55:07 +0200

    First working version

--- bladeburner.js
+++ bladeburner.js
@@ -1,19 +1,44 @@
-// import { runAndWait } from "helpers.js";
+import { runAndWait } from "helpers.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
-	if (!ns.bladeburner.joinBladeburnerDivision()) {
-		ns.tprintf("Not in Bladeburners division");
+	ns.disableLog("sleep");
+	const player = ns.getPlayer();
+	if (player.bitNodeN != 6 && player.bitNodeN != 7) {
+		ns.tprintf("Neither on bitnode 6 or 7 (%d)", player.bitNodeN);
 		return;
 	}
-	ns.tprintf("Joined Bladeburners division");
-	while (!ns.bladeburner.joinBladeburnerFaction()) {
-		ns.tprintf("Not in Bladeburners faction");
-		await ns.sleep(10000);
-		return;
-	}
-	ns.tprintf("Joined Bladeburners faction");
+	await prepareAndJoin(ns);
 	await runContracts(ns);
+}
+
+async function prepareAndJoin(ns) {
+	var player = ns.getPlayer();
+	while (!player.inBladeburner) {
+		ns.stopAction();
+		await runAndWait(ns, "commit-crimes.js", "--until_stats", "100", "--timed", "120");
+		player = ns.getPlayer();
+		if (player.strength < 100) {
+			await workout(ns, "Strength");
+		}
+		if (player.agility < 100) {
+			await workout(ns, "Agility");
+		}
+		if (player.defense < 100) {
+			await workout(ns, "Defense");
+		}
+		if (player.dexterity < 100) {
+			await workout(ns, "Dexterity");
+		}
+		if (ns.bladeburner.joinBladeburnerDivision()) {
+			break;
+		}
+	}
+}
+
+/** @param {NS} ns */
+async function workout(ns, stat) {
+	await runAndWait(ns, "workout.js", stat, ns.isFocused());
 }
 
 /** @param {NS} ns */
@@ -33,15 +58,32 @@ async function runContracts(ns) {
 					bestChance = chances[0];
 					bestContract = contract;
 				}
-				if (bestContract) {
-					ns.bladeburner.startAction("Contract", contract);
-				}
 			}
+			if (bestContract) {
+				var { type, name } = ns.bladeburner.getCurrentAction();
+				if (type != "Contract" || name != bestContract) {
+					ns.printf("Current type %s, action %s, changing to %s", type, name, bestContract);
+					ns.bladeburner.stopBladeburnerAction();
+					ns.bladeburner.startAction("Contract", bestContract);
+				}
+				await ns.sleep(ns.bladeburner.getActionTime("Contract", bestContract));
+			}
+		} else {
+			var { type, name } = ns.bladeburner.getCurrentAction();
+			if (type != "General" || name != "Training") {
+				ns.printf("Current type %s, action %s, changing to Training", type, name);
+				ns.bladeburner.stopBladeburnerAction();
+				ns.bladeburner.startAction("General", "Training");
+			}
+			await ns.sleep(ns.bladeburner.getActionTime("General", "Training"));
 		}
-		await ns.sleep(1000);
-		while (ns.bladeburner.getCurrentAction().type != "Idle") {
-			ns.tprintf("Doing %s", ns.bladeburner.getCurrentAction().name);
-			await ns.sleep(1000);
+		await ns.sleep(500);
+		for (var skill of ns.bladeburner.getSkillNames()) {
+			if (ns.bladeburner.getSkillUpgradeCost(skill) <= ns.bladeburner.getSkillPoints()) {
+				ns.tprintf("Spending %d skillpoints on %s",
+					ns.bladeburner.getSkillUpgradeCost(skill), skill)
+				ns.bladeburner.upgradeSkill(skill);
+			}
 		}
 	}
 }
