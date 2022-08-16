df89a4d0d0670117f1c27614c6cd7fd16d400ef3
Holger Oehm <holger.oehm@holger-oehm.de>
2022-08-16 12:28:37 +0200

    Prefer money over other stuff as long as it is scarce

--- spend-hashes.js
+++ spend-hashes.js
@@ -1,3 +1,5 @@
+import { getAvailableMoney } from "helpers.js";
+
 const money = "Sell for Money";
 const blade_rank = "Exchange for Bladeburner Rank";
 const blade_skill = "Exchange for Bladeburner SP";
@@ -7,11 +9,17 @@ const corp_research = "Exchange for Corporation Research";
 /** @param {NS} ns */
 export async function main(ns) {
 	const options = ns.flags([["all", false]]);
-	for (var upgName of [blade_rank, blade_skill, corp_money, corp_research]) {
-		await spendOn(ns, upgName);
-	}
-	if (options.all || ns.hacknet.numHashes() > 0.75 * ns.hacknet.hashCapacity()) {
+	if (!ns.getPlayer().hasCorporation || getAvailableMoney(ns, true) < 10e9) {
+		// we need this money
 		await spendOn(ns, money);
+	} else {
+		// we'll get the money from the corporation, use the hashes for other stuff
+		for (var upgName of [blade_rank, blade_skill, corp_money, corp_research]) {
+			await spendOn(ns, upgName);
+		}
+		if (options.all || ns.hacknet.numHashes() > 0.75 * ns.hacknet.hashCapacity()) {
+			await spendOn(ns, money);
+		}
 	}
 }
 
