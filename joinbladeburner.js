d328b8fad51c73ae79b2a30ea7ab27d73d93ab45
Holger Oehm <holger.oehm@holger-oehm.de>
2022-09-15 17:42:20 +0200

    Allow joining bladeburner on other nodes than 6 or 7

--- joinbladeburner.js
+++ joinbladeburner.js
@@ -1,33 +1,47 @@
+import * as c from "constants.js";
 import { runAndWait } from "helpers.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
+	const options = ns.flags([["division", false], ["faction", false]]);
 	var player = ns.getPlayer();
-	if (player.bitNodeN != 6 && player.bitNodeN != 7) {
-		ns.tprintf("Neither on bitnode 6 or 7 (%d)", player.bitNodeN);
-		return;
+	if (options.division) {
+		while (!player.inBladeburner) {
+			await runAndWait(ns, "commit-crimes.js");
+			await ns.sleep(60000);
+			player = ns.getPlayer();
+			if (player.skills.strength < 100) {
+				await workout(ns, "Strength");
+				await ns.sleep(15000);
+			}
+			if (player.skills.agility < 100) {
+				await workout(ns, "Agility");
+				await ns.sleep(15000);
+			}
+			if (player.skills.defense < 100) {
+				await workout(ns, "Defense");
+				await ns.sleep(15000);
+			}
+			if (player.skills.dexterity < 100) {
+				await workout(ns, "Dexterity");
+				await ns.sleep(15000);
+			}
+			if (ns.bladeburner.joinBladeburnerDivision()) {
+				ns.printf("Joined Bladeburners division");
+				ns.singularity.stopAction();
+				break;
+			}
+		}
 	}
-	while (!player.inBladeburner) {
-		ns.singularity.stopAction();
-		await runAndWait(ns, "commit-crimes.js", "--until_stats", "100", "--timed", "120");
-		player = ns.getPlayer();
-		if (player.skills.strength < 100) {
-			await workout(ns, "Strength");
-		}
-		if (player.skills.agility < 100) {
-			await workout(ns, "Agility");
-		}
-		if (player.skills.defense < 100) {
-			await workout(ns, "Defense");
-		}
-		if (player.skills.dexterity < 100) {
-			await workout(ns, "Dexterity");
-		}
-		if (ns.bladeburner.joinBladeburnerDivision()) {
-			break;
+	player = ns.getPlayer();
+	if (options.faction) {
+		if (player.inBladeburner &&
+			!player.factions.includes(c.BLADEBURNERS)) {
+			if (ns.bladeburner.joinBladeburnerFaction()) {
+				ns.printf("Joined Bladeburners faction");
+			}
 		}
 	}
-	ns.singularity.stopAction();
 }
 
 /** @param {NS} ns */
