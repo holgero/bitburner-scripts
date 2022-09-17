14efb6c12be73d5c8058678ff0737a61eb70a180
Holger Oehm <holger.oehm@holger-oehm.de>
2022-09-17 14:32:49 +0200

    Implement multiplier printing

--- playerinfo.js
+++ playerinfo.js
@@ -2,7 +2,7 @@ import { statsGainFactor } from "/helpers.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-  const options = ns.flags([["raw", false]], [["multiplier", false]]);
+  const options = ns.flags([["raw", false], ["multiplier", false]]);
   const playerInfo = ns.getPlayer();
   if (options.raw) {
     ns.tprintf("%s", JSON.stringify(playerInfo));
@@ -25,10 +25,40 @@ export async function main(ns) {
   }
   ns.tprintf("%30s: %s", "Factions", playerInfo.factions.join(", "));
   ns.tprintf("%30s: %s", "Jobs", JSON.stringify(playerInfo.jobs));
+  ns.tprintf("%30s: %s", "Work", JSON.stringify(ns.singularity.getCurrentWork()));
+  
   if (options.multiplier) {
     ns.tprintf("%30s", "Multiplier");
-    ns.tprintf("%30s: %s", "Hacking chance", playerInfo.mults.hacking_chance);
-    ns.tprintf("%30s: %s", "Hacking speed", playerInfo.mults.hacking_speed);
+    ns.tprintf("%30s: %s", "Hacking chance", playerInfo.mults.hacking_chance.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacking speed", playerInfo.mults.hacking_speed.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacking money", playerInfo.mults.hacking_money.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacking grow", playerInfo.mults.hacking_grow.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacking", playerInfo.mults.hacking.toFixed(2));
+    ns.tprintf("%30s: %s", "Strength", playerInfo.mults.strength.toFixed(2));
+    ns.tprintf("%30s: %s", "Defense", playerInfo.mults.defense.toFixed(2));
+    ns.tprintf("%30s: %s", "Dexterity", playerInfo.mults.dexterity.toFixed(2));
+    ns.tprintf("%30s: %s", "Agility", playerInfo.mults.agility.toFixed(2));
+    ns.tprintf("%30s: %s", "Charisma", playerInfo.mults.charisma.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacking exp", playerInfo.mults.hacking_exp.toFixed(2));
+    ns.tprintf("%30s: %s", "Strength exp", playerInfo.mults.strength_exp.toFixed(2));
+    ns.tprintf("%30s: %s", "Defense exp", playerInfo.mults.defense_exp.toFixed(2));
+    ns.tprintf("%30s: %s", "Dexterity exp", playerInfo.mults.dexterity_exp.toFixed(2));
+    ns.tprintf("%30s: %s", "Agility exp", playerInfo.mults.agility_exp.toFixed(2));
+    ns.tprintf("%30s: %s", "Charisma exp", playerInfo.mults.charisma_exp.toFixed(2));
+    ns.tprintf("%30s: %s", "Company rep", playerInfo.mults.company_rep.toFixed(2));
+    ns.tprintf("%30s: %s", "Faction rep", playerInfo.mults.faction_rep.toFixed(2));
+    ns.tprintf("%30s: %s", "Crime money", playerInfo.mults.crime_money.toFixed(2));
+    ns.tprintf("%30s: %s", "Crime success", playerInfo.mults.crime_success.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacknet node money", playerInfo.mults.hacknet_node_money.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacknet node purchase cost", playerInfo.mults.hacknet_node_purchase_cost.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacknet node ram cost", playerInfo.mults.hacknet_node_ram_cost.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacknet node core cost", playerInfo.mults.hacknet_node_core_cost.toFixed(2));
+    ns.tprintf("%30s: %s", "Hacknet node level cost", playerInfo.mults.hacknet_node_level_cost.toFixed(2));
+    ns.tprintf("%30s: %s", "Work money", playerInfo.mults.work_money.toFixed(2));
+    ns.tprintf("%30s: %s", "Bladeburner max stamina", playerInfo.mults.bladeburner_max_stamina.toFixed(2));
+    ns.tprintf("%30s: %s", "Bladeburner stamina gain", playerInfo.mults.bladeburner_stamina_gain.toFixed(2));
+    ns.tprintf("%30s: %s", "Bladeburner analysis", playerInfo.mults.bladeburner_analysis.toFixed(2));
+    ns.tprintf("%30s: %s", "Bladeburner success chance", playerInfo.mults.bladeburner_success_chance.toFixed(2));
   }
   ns.tprintf("%30s: %s", "Karma:", ns.heart.break());
   ns.tprintf("%30s: %f", "Stats gain factor", statsGainFactor(ns));
