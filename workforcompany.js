37b2842cb0b5215f4c61dd9a9d7dd071cf5d4fa4
Holger Oehm <holger.oehm@holger-oehm.de>
2022-09-20 11:15:55 +0200

    Apply for promotions while working for a company

--- workforcompany.js
+++ workforcompany.js
@@ -3,19 +3,17 @@ export async function main(ns) {
 	var company = ns.args[0];
 	var job = ns.args[1];
 
+	if (ns.singularity.applyToCompany(company, job)) {
+		ns.tprintf("Applied successfully at %s for %s job", company, job);
+	}
 	const current = ns.singularity.getCurrentWork();
 	if (current != null && current.type == "COMPANY" && current.companyName == company) {
 		ns.printf("Already working for %s", current.companyName);
 		toastCompletion(ns, company);
 		return;
 	}
-	for (var ii = 0; ii < 3; ii++) {
-		ns.singularity.applyToCompany(company, job);
-		if (ns.singularity.workForCompany(company)) {
-			toastCompletion(ns, company);
-			break;
-		}
-		await ns.sleep(5000);
+	if (ns.singularity.workForCompany(company)) {
+		ns.tprintf("Now working at %s as %s", company, job);
 	}
 }
 
