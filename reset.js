3212e453d7516831707f2cc67ed49a3b25c87083
Holger Oehm <holger.oehm@holger-oehm.de>
2022-09-25 08:12:06 +0200

    Print a countdown before performing the reset

--- reset.js
+++ reset.js
@@ -1,5 +1,9 @@
 /** @param {NS} ns **/
 export async function main(ns) {
+	for (var countDown = 10; countDown >0; countDown--) {
+		ns.tprintf("Reset in %d seconds", countDown);
+		await ns.sleep(1000);
+	}
 	ns.singularity.installAugmentations("nodestart.js");
 	ns.singularity.softReset("nodestart.js");
 }
