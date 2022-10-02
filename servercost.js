2c4ba3f28eea73ca9380ca33601116174c834ab6
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-02 11:27:50 +0200

    Consider bitnode multiplier for information about servers to buy

--- servercost.js
+++ servercost.js
@@ -1,7 +1,8 @@
-import { getAvailableMoney, formatMoney } from "helpers.js";
+import { getAvailableMoney, getDatabase, formatMoney } from "helpers.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
+	const database = getDatabase(ns);
 	if (ns.getPurchasedServerLimit() <= 0) {
 		ns.tprintf("Cannot buy any servers.");
 		return;
@@ -10,6 +11,13 @@ export async function main(ns) {
 		ns.tprintf("Current memory of server pserv-0: %d GB", ns.getServerMaxRam("pserv-0"));
 	}
 	var money = getAvailableMoney(ns);
+	if (database.bitnodemultipliers) {
+		const multiplier = database.bitnodemultipliers.ServerMaxMoney;
+		if (multiplier < 0.25) {
+			ns.tprintf("Reducing money to spend according to ServerMaxMoney (%s)", multiplier);
+			money *= multiplier;
+		}
+	}
 	var havePerServer = money / ns.getPurchasedServerLimit();
 	var ram = 64;
 	for (var i = 0; i < 16; i++) {
@@ -36,6 +44,6 @@ export async function main(ns) {
 function printInfo(ns, ram) {
 	ns.tprintf("%d GB ram, costs: %s total, %s per server",
 		ram,
-		formatMoney(ns.getPurchasedServerCost(ram)*ns.getPurchasedServerLimit()),
+		formatMoney(ns.getPurchasedServerCost(ram) * ns.getPurchasedServerLimit()),
 		formatMoney(ns.getPurchasedServerCost(ram)));
 }
