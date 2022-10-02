28dd2968fca49d7d1cc401c40323e573678e1ba5
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-02 07:28:55 +0200

    Clean up obsolete files

--- database.js
+++
@@ -1,5 +0,0 @@
-/** @param {NS} ns */
-export function xgetAugmentationPrice(ns, augmentation) {
-	const database = JSON.parse(ns.read("database.txt"));
-	return database.augmentations.find(aug => aug.name == augmentation).price;
-}
