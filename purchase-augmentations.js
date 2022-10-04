afd00cee476e5687fa78931275badce92d637910
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-04 19:06:25 +0200

    Fix flag for purchase cores and purchase ram, also remove
     optional reset logic

--- purchase-augmentations.js
+++ purchase-augmentations.js
@@ -5,14 +5,12 @@ import { GOVERNOR } from "constants.js";
 export async function main(ns) {
 	var options = ns.flags([["reboot", false]]);
 	var toPurchase = JSON.parse(options._[0]);
-	var needReset = false;
 
 	ns.tprintf("Augmentations to buy: %v", toPurchase);
 
 	for (var augmentation of toPurchase) {
 		for (var faction of ns.getPlayer().factions) {
 			if (ns.singularity.getAugmentationsFromFaction(faction).includes(augmentation)) {
-				needReset = true;
 				if (ns.singularity.purchaseAugmentation(faction, augmentation)) break;
 			}
 		}
@@ -20,13 +18,12 @@ export async function main(ns) {
 
 	ns.tprintf("Bought planned augmentations, spending remaining money: ", getAvailableMoney(ns, true));
 	// if there is money left, run home upgrades
-	await runAndWait(ns, "purchase-cores.js", "unlimited");
-	await runAndWait(ns, "purchase-ram.js", "unlimited");
+	await runAndWait(ns, "purchase-cores.js", "--unlimited");
+	await runAndWait(ns, "purchase-ram.js", "--unlimited");
 	await runAndWait(ns, "governors.js");
 	await runAndWait(ns, "purchase-sleeve-augs.js");
 	
 	if (options.reboot) {
-		if (needReset) ns.spawn("reset.js");
-		ns.spawn("nodestart.js");
+		ns.spawn("reset.js");
 	}
 }
