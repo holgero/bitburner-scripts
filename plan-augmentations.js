5b36a321274b5dfe03e51c3abb5359c712ad040f
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-04 11:39:38 +0200

    Use external file to buy governors

--- plan-augmentations.js
+++ plan-augmentations.js
@@ -26,18 +26,9 @@ export async function main(ns) {
 	filterExpensiveAugmentations(ns, toPurchase, haveMoney, ["Hacking", "Bladeburner"]);
 	const augNames = toPurchase.map(a => a.name);
 
-	var governor_faction = factions[0].name;
-	var maxRep = 0;
-	for (var faction of factions) {
-		if (faction.name != BLADEBURNERS && !faction.gang && (faction.reputation > maxRep)) {
-			governor_faction = faction.name;
-			maxRep = faction.reputation;
-		}
-	}
 	ns.tprintf("Augmentations to purchase: %s", JSON.stringify(augNames));
-	ns.tprintf("Governor faction is %s (with rep %d)", governor_faction, maxRep);
 	if (options.run_purchase) {
 		ns.spawn("purchase-augmentations.js", 1, JSON.stringify(augNames),
-			governor_faction, "--reboot", JSON.stringify(options));
+			"--reboot", JSON.stringify(options));
 	}
 }
--- purchase-augmentations.js
+++ purchase-augmentations.js
@@ -5,11 +5,9 @@ import { GOVERNOR } from "constants.js";
 export async function main(ns) {
 	var options = ns.flags([["reboot", false]]);
 	var toPurchase = JSON.parse(options._[0]);
-	var governor_faction = options._[1];
 	var needReset = false;
 
 	ns.tprintf("Augmentations to buy: %v", toPurchase);
-	ns.tprintf("Faction to buy governors from: %s", governor_faction);
 
 	for (var augmentation of toPurchase) {
 		for (var faction of ns.getPlayer().factions) {
@@ -24,19 +22,9 @@ export async function main(ns) {
 	// if there is money left, run home upgrades
 	await runAndWait(ns, "purchase-cores.js", "unlimited");
 	await runAndWait(ns, "purchase-ram.js", "unlimited");
-
-	// spend the rest of the money on Neural Governor augs
-	while (getAvailableMoney(ns, true) > ns.singularity.getAugmentationPrice(GOVERNOR)) {
-		if (ns.singularity.purchaseAugmentation(governor_faction, GOVERNOR)) {
-			needReset = true;
-			ns.tprintf("Bought governor, money left: %d", getAvailableMoney(ns, true));
-			await ns.sleep(500);
-		} else {
-			break;
-		}
-	}
-
+	await runAndWait(ns, "governors.js");
 	await runAndWait(ns, "purchase-sleeve-augs.js");
+	
 	if (options.reboot) {
 		if (needReset) ns.spawn("reset.js");
 		ns.spawn("nodestart.js");
