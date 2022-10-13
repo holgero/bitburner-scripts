ee8d0d3dcaa78a5a73069854e6f4459627769177
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-13 19:28:22 +0200

    Make database creation more robust against low memory ...
    letting a single part fail

--- create-database.js
+++ create-database.js
@@ -2,11 +2,14 @@ import { runAndWait } from "helpers.js";
 
 /** @param {NS} ns **/
 export async function main(ns) {
-	await runAndWait(ns, "create-database1.js");
-	await runAndWait(ns, "create-database2.js");
-	await runAndWait(ns, "create-database3.js");
-	await runAndWait(ns, "create-database4.js");
-	await runAndWait(ns, "create-database5.js");
-	await runAndWait(ns, "create-database6.js");
-	await runAndWait(ns, "create-database7.js");
+	while (!(await runAndWait(ns, "create-database1.js") &&
+		await runAndWait(ns, "create-database2.js") &&
+		await runAndWait(ns, "create-database3.js") &&
+		await runAndWait(ns, "create-database4.js") &&
+		await runAndWait(ns, "create-database5.js") &&
+		await runAndWait(ns, "create-database6.js") &&
+		await runAndWait(ns, "create-database7.js"))) {
+		ns.tprintf("Failed to create database!");
+		await ns.sleep(1000);
+	}
 }
