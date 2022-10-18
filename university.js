ab39b262c68fc39b3097e5682babb48a84aaaf84
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-18 12:18:04 +0200

    Fix continiously restarting computer courses

--- university.js
+++ university.js
@@ -1,22 +1,22 @@
 import { getAvailableMoney } from "./helpers.js";
 
 const UNIS = ["ZB Institute of Technology", "Summit University", "Rothman University"];
-const CS_COURSES = ["Algorithms", "Networks", "Data Structures", "Study Computer Science"];
+const CS_COURSES = ["ALGORITHMS", "Networks", "Data Structures", "Study Computer Science"];
 const CHEAP_CS_COURSES = ["Study Computer Science"];
 const BA_COURSES = ["Leadership", "Management"];
 
 /** @param {NS} ns */
 export async function main(ns) {
 	const options = ns.flags([["course", "CS"], ["focus", "false"], ["negative", false]]);
-	const focus = JSON.parse(options.focus);
 	var courses;
 	var classType;
 	switch (options.course) {
 		case "CS":
-			classType = "STUDYCOMPUTERSCIENCE";
 			if (getAvailableMoney(ns) > 1e9 || options.negative) {
+				classType = "ALGORITHMS";
 				courses = CS_COURSES;
 			} else {
+				classType = "STUDYCOMPUTERSCIENCE";
 				courses = CHEAP_CS_COURSES;
 			}
 			break;
@@ -24,15 +24,16 @@ export async function main(ns) {
 			courses = BA_COURSES;
 			break;
 	}
+	const current = ns.singularity.getCurrentWork();
+	if (current != null && current.type == "CLASS" && current.classType == classType) {
+		ns.printf("Already %s", current.classType);
+		return;
+	} else {
+		ns.tprintf("%s", JSON.stringify(current));
+	}
 	for (var uni of UNIS) {
 		for (var course of courses) {
-			// {"type":"CLASS","cyclesWorked":226,"classType":"STUDYCOMPUTERSCIENCE","location":"Rothman University"}
-			const current = ns.singularity.getCurrentWork();
-			if (current != null && current.type == "CLASS" && current.classType == classType) {
-				ns.printf("Already %s", current.classType);
-				return;
-			}
-			if (ns.singularity.universityCourse(uni, course, focus)) {
+			if (ns.singularity.universityCourse(uni, course)) {
 				return;
 			}
 		}
