91ca7c631fbedcbdea853419754300f02e8e891b
Holger Oehm <holger.oehm@holger-oehm.de>
2022-10-25 09:31:58 +0200

    Go back to previously not finished bitnodes

--- destroy-world.js
+++ destroy-world.js
@@ -1,39 +1,44 @@
 /** @param {NS} ns */
 export async function main(ns) {
+	const options = ns.flags([["dry-run", false]]);
 	const bitNodeN = ns.getPlayer().bitNodeN;
 	const owned = ns.singularity.getOwnedSourceFiles();
+	const nextNode = nextBitnode(ns, bitNodeN, owned);
 	const thisNode = owned.find(a => a.n == bitNodeN);
 	const thisLevel = thisNode ? thisNode.lvl : 0;
-	var nextBitnode = bitNodeN;
-	if (thisLevel >= 2) {
-		// we are on the third run through this bitnode
-		if (thisNode.n != 12) {
-			// bitnode 12 is recursion, just stay until a manual intervention
-			nextBitnode++;
-		}
-	}
 	ns.tprintf("Destroying world daemon on bitNode %d.%d, proceeding on bitNode %d",
-		bitNodeN, thisLevel, nextBitnode);
+		bitNodeN, thisLevel, nextNode);
+	if (options["dry-run"]) {
+		return;
+	}
 	for (var countDown = 10; countDown > 0; countDown--) {
 		ns.tprintf("End of the world in %d seconds", countDown);
 		await ns.sleep(1000);
 	}
-	if (ns.singularity.destroyW0r1dD43m0n(nextBitnode, "nodestart.js")) {
+	if (ns.singularity.destroyW0r1dD43m0n(nextNode, "nodestart.js")) {
 		ns.tprintf("World destroyed");
 		return;
 	}
 	ns.tprintf("Failed to destroy world daemon");
 }
-/*
+
+/** @param {NS} ns */
 function nextBitnode(ns, current, owned) {
-	owned.sort((a,b)=>a.n-b.n);
-	for (var ii=0;ii<=current;ii++) {
-		if (owned[ii].n > ii) {
-			return ii;
+	const thisNode = owned.find(a => a.n == current);
+	if (thisNode.lvl < 2) {
+		return current;
+	}
+	if (thisNode.n == 12 && thisNode.lvl < 50) {
+		return current;
+	}
+	owned.sort((a, b) => a.n - b.n);
+	for (var ii = 0; ii < owned.length; ii++) {
+		if (owned[ii].n > ii + 1) {
+			return ii + 1;
 		}
-		if (owned[ii])
+		if (owned[ii].n != current && owned[ii].lvl < 3) {
+			return ii + 1;
+		}
 	}
-	if (owned.find(a=>a.n == 1 && a.lvl < 3)) {
-		return 1;
-	}
-} */
+	return owned.length + 1;
+}
