9f9706d757a497d525f013894900ae6cd20c84c2
Holger Oehm <holger.oehm@holger-oehm.de>
2022-11-03 11:34:33 +0100

    Improve donating to factions: Only donate if 80% of goal can be reached
    in this way money is still available for investments

--- donate-faction.js
+++ donate-faction.js
@@ -1,10 +1,14 @@
+import { formatMoney } from "helpers.js";
+
 /** @param {NS} ns **/
 export async function main(ns) {
-	var faction = ns.args[0];
-	var reputation = ns.args[1];
-	var maximum = ns.args[2];
-	var money = costToBribeTo(ns, faction, reputation);
-	ns.singularity.donateToFaction(faction, Math.min(money, maximum));
+	const faction = ns.args[0];
+	const reputation = ns.args[1];
+	const maximum = ns.args[2];
+	if (reachableReputationPercentage(ns, faction, reputation, maximum) > 0.8) {
+		const money = costToBribeTo(ns, faction, reputation);
+		ns.singularity.donateToFaction(faction, Math.min(money, maximum));
+	}
 }
 
 /** @param {NS} ns **/
@@ -13,6 +17,14 @@ function costToBribeTo(ns, faction, reputation) {
 		return 0;
 	}
 	var needed = Math.max(0, reputation - ns.singularity.getFactionRep(faction));
-	var cost = 1000000 * needed / ns.getPlayer().mults.faction_rep;
+	var cost = 1e6 * needed / ns.getPlayer().mults.faction_rep;
 	return cost;
+}
+
+/** @param {NS} ns **/
+function reachableReputationPercentage(ns, faction, reputation, money) {
+	const reachable = ns.singularity.getFactionRep(faction) + money * ns.getPlayer().mults.faction_rep / 1e6;
+	ns.tprintf("Can reach %s reputation (%s %%) with %s by donating %s",
+	 Math.floor(reachable), (100*reachable/reputation).toFixed(1), faction, formatMoney(money));
+	return reachable / reputation;
 }
