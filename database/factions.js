4d6826ca1a98cc57ee223cf331ba9bfe93a7fe04
Holger Oehm <holger.oehm@holger-oehm.de>
2022-11-08 12:12:02 +0100

    Filter out bladeburners and stanek if the feature is not available

--- database/factions.js
+++ database/factions.js
@@ -3,21 +3,27 @@ import * as c from "constants.js";
 /** @param {NS} ns **/
 export async function main(ns) {
 	const database = JSON.parse(ns.read("database.txt"));
-	buildFactionAugmentations(ns, database.factions, database.owned_augmentations);
+	buildFactionAugmentations(ns, database);
 	ns.write("database.txt", JSON.stringify(database), "w");
 }
 
 /** @param {NS} ns **/
-function buildFactionAugmentations(ns, factions, owned_augmentations) {
-	var ignore = owned_augmentations.slice(0);
+function buildFactionAugmentations(ns, database) {
+	var ignore = database.owned_augmentations.slice(0);
 	ignore.push(c.GOVERNOR);
 	for (var faction_name of c.ALL_FACTIONS) {
+		if (faction_name == c.BLADEBURNERS && !database.features.includes("bladeburners")) {
+			continue;
+		}
+		if (faction_name == c.CHURCH && !database.features.includes("church")) {
+			continue;
+		}
 		var faction = c.STORY_LINE.find(a => a.name == faction_name);
 		if (!faction) {
 			faction = { name: faction_name }
 		}
 		var augmentations = ns.singularity.getAugmentationsFromFaction(faction_name).
 			filter(a => !ignore.includes(a));
-		factions.push({ ...faction, augmentations: augmentations });
+		database.factions.push({ ...faction, augmentations: augmentations });
 	}
 }
