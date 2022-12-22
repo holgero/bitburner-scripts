70a5c63f9166a990a0697b6c4a1f5aa132a3259a
Holger Oehm <holger.oehm@holger-oehm.de>
2022-12-22 18:42:28 +0100

    Improve printing corporation info, no longer print undefd stuff

--- corpinfo.js
+++ corpinfo.js
@@ -3,12 +3,17 @@ import { formatMoney, getCorporationInfo } from "./helpers.js";
 /** @param {NS} ns */
 export async function main(ns) {
 	const corporationInfo = getCorporationInfo(ns);
+	if (!corporationInfo.name) {
+		ns.tprintf("No corporation");
+		return;
+	}
 	const low = corporationInfo.valuation / (2 * corporationInfo.totalShares);
 	const high = corporationInfo.valuation / (2 * (corporationInfo.totalShares -
 		corporationInfo.issuedShares - corporationInfo.numShares) + 1);
 	const profit = corporationInfo.revenue - corporationInfo.expenses;
 
-	ns.tprintf("Corporation: share=%s (%s-%s), funds=%s, profit=%s, cool=%d s, bonus time=%d s, owned=%s",
+	ns.tprintf("Corporation %s: share=%s (%s-%s), funds=%s, profit=%s, cool=%d s, bonus time=%d s, owned=%s",
+		corporationInfo.name,
 		formatMoney(corporationInfo.sharePrice), formatMoney(low), formatMoney(high),
 		formatMoney(corporationInfo.funds),
 		formatMoney(profit),
