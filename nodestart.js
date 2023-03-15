6a1e5d6563557ae3d6bbd0f995c201efaac62376
Holger Oehm <holger.oehm@holger-oehm.de>
2023-03-15 12:11:27 +0100

    Increase requirements how near to fulfill stats and hacking level before
    making money available to join a faction

--- nodestart.js
+++ nodestart.js
@@ -468,13 +468,13 @@ async function meetMoneyGoals(ns) {
 		// return;
 	}
 	for (var goal of goals.factionGoals.filter(a => a.money && !player.factions.includes(a.name))) {
-		if (goal.hack && ns.getPlayer().skills.hacking < 0.98 * goal.hack) {
+		if (goal.hack && ns.getPlayer().skills.hacking < 0.999 * goal.hack) {
 			continue;
 		}
 		if (goal.stats) {
 			const minStat = Math.min(player.skills.strength, player.skills.dexterity,
 				player.skills.defense, player.skills.agility);
-			if (minStat < 0.98 * goal.stats) {
+			if (minStat < 0.999 * goal.stats) {
 				continue;
 			}
 		}
