3953b8a9be1f1c0327a2742747608c6ff5ac15a1
Holger Oehm <holger.oehm@holger-oehm.de>
2023-08-02 09:55:12 +0200

    Add a last-moment run of spend-hashes, governors and
    purchase-sleeve-augs, to not waste hashes and money before reset

--- reset.js
+++ reset.js
@@ -1,9 +1,14 @@
+import { runAndWait } from "helpers.js";
+
 /** @param {NS} ns **/
 export async function main(ns) {
 	for (var countDown = 10; countDown >0; countDown--) {
 		ns.tprintf("Reset in %d seconds", countDown);
 		await ns.sleep(1000);
 	}
+	await runAndWait(ns, "spend-hashes.js", "--all");
+	await runAndWait(ns, "governors.js");
+	await runAndWait(ns, "purchase-sleeve-augs.js");
 	ns.singularity.installAugmentations("nodestart.js");
 	ns.singularity.softReset("nodestart.js");
 }
