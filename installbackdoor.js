381f7cd7e27fe196f60d6a529ba99fa7fcc4b02a
Holger Oehm <holger.oehm@holger-oehm.de>
2023-12-12 09:47:38 +0100

    Reduce noise while backdooring servers

--- installbackdoor.js
+++ installbackdoor.js
@@ -3,11 +3,11 @@ export async function main(ns) {
 	const path = JSON.parse(ns.args[0]);
 	for (var host of path) {
 		ns.singularity.connect(host);
-		ns.tprintf("Connected to %s", host);
+		ns.printf("Connected to %s", host);
 	}
-	ns.tprintf("Installing backdoor on %s", host);
+	ns.printf("Installing backdoor on %s", host);
 	await ns.singularity.installBackdoor();
-	ns.tprintf("Backdoor installed");
+	ns.tprintf("Backdoor installed on %s", host);
 	path.reverse();
 	for (var host of path) {
 		ns.singularity.connect(host);
