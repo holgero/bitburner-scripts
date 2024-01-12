c469874908f290a638404fa20a04d02fdf2c32fd
Holger Oehm <holger.oehm@holger-oehm.de>
2024-01-12 09:11:30 +0100

    Increase the time to wait for logs to appear

--- showlogs.js
+++ showlogs.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	const name = ns.args[0];
-	for (var ii = 0; ii < 10; ii++) {
+	for (var ii = 0; ii < 60; ii++) {
 		const recent = ns.getRecentScripts().filter(a => a.filename == name);
 		if (recent.length > 0) {
 			ns.tprintf("%s", recent[0].logs.join("\n"));
